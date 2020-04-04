f2a1d6d80f872426e197070734bce331f3003139
linahammadi <linaajeel@gmail.com>
2020-04-04 17:48:36 +0000

    final

---
+++ my-app/build/precache-manifest.2e9843bde59eadb1970f7d08ec58a5f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07c6ea6c38e46706eedca60071221988",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6388ecbabc461c4dc938",
+    "url": "/static/css/main.900559b3.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "/static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6388ecbabc461c4dc938",
+    "url": "/static/js/main.144e076d.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "1c65444d180e2ea1a38abaa9f658c436",
+    "url": "/static/media/download.1c65444d.png"
+  }
+]);
